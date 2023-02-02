9d0dffaefbafcdbd6ecdccc577654f0e2bb95c0e
gthomas513 <gabe.thomas513@gmail.com>
2023-02-02 12:19:50 -0500

    pushing functional webapp
    - needs design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { StytchLogin } from '@stytch/react';
+import { useStytch } from '@stytch/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const Login = () => {
+  const stytchClient = useStytch();
+
+  const sendEmailMagicLink = () => {
+    stytchClient.magicLinks.email.send('gabe.thomas513@gmail.com', {
+      login_magic_link_url: 'http:localhost:3000',
+      login_expiration_minutes: 5,
+    });
+  };
+              
+  const config = {
+  "products": [
+    "emailMagicLinks",
+    "otp"
+  ],
+  "emailMagicLinksOptions": {
+    "loginRedirectURL": "http://localhost:3000",
+    "loginExpirationMinutes": 30,
+    "signupRedirectURL": "http://localhost:3000",
+    "signupExpirationMinutes": 30
+  },
+
+  "otpOptions": {
+    "methods": [
+      "sms"
+    ],
+    "expirationMinutes": 5
+  }
+};
+  const styles = {
+  "container": {
+    "backgroundColor": "#FFFFFF",
+    "borderColor": "#ADBCC5",
+    "borderRadius": "8px",
+    "width": "400px"
+  },
+  "colors": {
+    "primary": "#19303D",
+    "secondary": "#5C727D",
+    "success": "#0C5A56",
+    "error": "#8B1214"
+  },
+  "buttons": {
+    "primary": {
+      "backgroundColor": "#19303D",
+      "textColor": "#FFFFFF",
+      "borderColor": "#19303D",
+      "borderRadius": "4px"
+    },
+    "secondary": {
+      "backgroundColor": "#FFFFFF",
+      "textColor": "#19303D",
+      "borderColor": "#19303D",
+      "borderRadius": "4px"
+    }
+  },
+  "fontFamily": "Arial",
+  "hideHeaderText": false,
+  "logo": {
+    "logoImageUrl": ""
+  }
 }
-
-export default App;
+                            
+  return <StytchLogin config={config} styles={styles} />;
+}
+              
+export default Login;
